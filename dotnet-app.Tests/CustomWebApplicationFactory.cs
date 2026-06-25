9c93544200bb164fe2eccf9d665575ca7b1af7a0
vicky-929 <vr2828816@gmail.com>
2026-06-25 12:51:49 +0000

    Add simple health endpoint test

---
+++ dotnet-app.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace dotnet_app.Tests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+}
--- dotnet-app/Program.cs
+++ dotnet-app/Program.cs
@@ -5,3 +5,7 @@ app.MapGet("/", () => "Hello from Blue/Green deployment! v1.0");
 app.MapGet("/health", () => Results.Ok(new { status = "healthy", version = "1.0" }));
 
 app.Run("http://0.0.0.0:5000");
+
+public partial class Program
+{
+}
